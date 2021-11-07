1e09a5e86dcb06d41b15208a42722e4709831427
Артём Виноградов <v_artem_2002@mail.ru>
2021-11-08 00:26:02 +0400

    Revert "pre"
    This reverts commit 7010545d11d123aa271ebf9ee79d60b07807fc02.

--- NodePractice/controllers/homeController.js
+++
@@ -1,6 +0,0 @@
-exports.index = function (request, response) {
-    response.send("Главная страница <br><a href='/users'>Пользователи</a><br><a href='/feedbacks'>Вопросы</a>");
-};
-exports.about = function (request, response) {
-    response.send("О сайте");
-};
